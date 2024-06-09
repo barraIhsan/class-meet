07d5c947b61f8cf6c06fa32e1f6b83967d55b266
barraIhsan <57800056+barraIhsan@users.noreply.github.com>
2024-06-09 18:53:19 +0700

    feat: add gray color palette to tailwind

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,13 +1,19 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
 	darkMode: "selector",
 	theme: {
 		extend: {
 			fontFamily: {
-				'sans': ["Inter", "Arial", "Helvetica", "sans-serif"]
-			}
+				sans: ["Inter", "Arial", "Helvetica", "sans-serif"],
+			},
+			colors: {
+				gray: {
+					100: "#4B4B4B",
+					200: "#1C1C1F"
+				}
+			},
 		},
 	},
 	plugins: [],
-}
+};
