80fda2bc16fa38bcac138be5b3870fdf416d10aa
barraIhsan <57800056+barraIhsan@users.noreply.github.com>
2024-06-10 21:35:16 +0700

    feat: add the actual tourism data

--- src/data/tourism.js
+++ src/data/tourism.js
@@ -1,26 +1,32 @@
 export const tourismItems = [
 	{
-		title: "Go! Wet",
-		content: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum consequuntur velit eos ut officiis sunt libero excepturi illo eligendi vitae.",
-		imgSrc: "",
+		title: "Grand Splash Water Park",
+		content: "Wisata taman air yang ada di dalam Komplek Grand Residence Bekasi dengan berbagai fasilitas dan wahana.",
+		imgSrc: "/class-meet/img/tourism1.png",
 		rate: 4.4
 	},
 	{
 		title: "Taman Kota Bekasi",
-		content: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum consequuntur velit eos ut officiis sunt libero excepturi illo eligendi vitae.",
-		imgSrc: "",
+		content: "Taman Kota Bekasi ditata dengan sangat cantik dan menarik yang membuatnya nyaman untuk dikunjungi.",
+		imgSrc: "/class-meet/img/tourism2.png",
 		rate: 4.5
 	},
 	{
 		title: "Gedung Juang 45 Bekasi",
-		content: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum consequuntur velit eos ut officiis sunt libero excepturi illo eligendi vitae.",
-		imgSrc: "",
+		content: "Gedung Juang 45 Bekasi adalah sebuah bangunan bersejarah yang menjadi saksi bisu perjuangan dan perkembangan Bekasi dari masa ke masa",
+		imgSrc: "/class-meet/img/tourism3.png",
 		rate: 4.6
 	},
 	{
-		title: "Go! Wet",
-		content: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum consequuntur velit eos ut officiis sunt libero excepturi illo eligendi vitae.",
-		imgSrc: "",
-		rate: 4.4
+		title: "Alun-alun Eduforest Bekasi",
+		content: "Alun-alun Edu Forest ini mengusung konsep ruang terbuka dengan perpaduan alam.",
+		imgSrc: "/class-meet/img/tourism4.png",
+		rate: 4.5
+	},
+	{
+		title: "Situ Hambaro",
+		content: "Keindahan Situ Rawa Hambaro merupakan salah satu Potensi alam yang sangat besar yang terletak di kecamatan Setu,",
+		imgSrc: "/class-meet/img/tourism5.png",
+		rate: 4.5
 	}
 ]
