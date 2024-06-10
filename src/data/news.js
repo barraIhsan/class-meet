0210375fb71becc1c7cb07a6ceb2d0807063ca7d
barraIhsan <57800056+barraIhsan@users.noreply.github.com>
2024-06-10 13:33:47 +0700

    feat: add news section
    And now all the section is wrapped inside a main that gives max-width.
    The news also get the data dynamically from js file

---
+++ src/data/news.js
@@ -0,0 +1,34 @@
+export const newsItems = [
+	{
+		title: "Title",
+		content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco.",
+		flexDirection: "col",
+		colSpan: 3,
+		rowSpan: 3,
+		imgSrc: ""
+	},
+	{
+		title: "Title",
+		content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco.",
+		flexDirection: "row-reverse",
+		colSpan: 5,
+		rowSpan: 2,
+		imgSrc: ""
+	},
+	{
+		title: "Title",
+		content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco.",
+		flexDirection: "row",
+		colSpan: 5,
+		rowSpan: 4,
+		imgSrc: ""
+	},
+	{
+		title: "Title",
+		content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco.",
+		flexDirection: "col",
+		colSpan: 3,
+		rowSpan: 3,
+		imgSrc: ""
+	}
+]
