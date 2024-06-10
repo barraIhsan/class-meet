175b30ad36de3333a22a5a6dc8fabd3ed63a3fac
barraIhsan <57800056+barraIhsan@users.noreply.github.com>
2024-06-10 14:33:08 +0700

    feat: add the news data
    Also add line-clamp to make the text not too long, adjust the font size
    and make the title underline.

--- src/data/news.js
+++ src/data/news.js
@@ -1,34 +1,42 @@
 export const newsItems = [
 	{
-		title: "Title",
-		content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco.",
+		title: "Begal Bersenjata Bacok Sopir di Bekasi, Pelaku Tertangkap Usai Tabrakan",
+		content: "Polisi kemudian mengembangkan kasus itu hingga menangkap dua pelaku lainnya. Satu pelaku masih diburu polisi.",
 		flexDirection: "col",
 		colSpan: 3,
 		rowSpan: 3,
-		imgSrc: ""
+		imgSrc: "/class-meet/img/news1.png",
+		year: 2,
+		link: "https://news.detik.com/berita/d-6116449/begal-bersenjata-bacok-sopir-di-bekasi-pelaku-tertangkap-usai-tabrakan"
 	},
 	{
-		title: "Title",
-		content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco.",
+		title: "PGI Sesalkan Pembongkaran Gereja HKBP Setu Bekasi",
+		content: "PGI menilai pembongkaran yang dilakukan Pemkab Bekasi hanya dikarenakan adanya tekanan pihak tertentu bukan menyalahi IMB.",
 		flexDirection: "row-reverse",
 		colSpan: 5,
 		rowSpan: 2,
-		imgSrc: ""
+		imgSrc: "/class-meet/img/news2.png",
+		year: 11,
+		link: "https://www.liputan6.com/video/read/618645/pgi-sesalkan-pembongkaran-gereja-hkbp-setu-bekasi"
 	},
 	{
-		title: "Title",
-		content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco.",
+		title: "Penemuan Janin Kucing Dikira Bayi Bikin Heboh Warga di Kabupaten Bekasi",
+		content: "Janin tersebut tergeletak di sekretariat RT di Desa Cijengkol, Kecamatan Setu, Kabupaten Bekasi.",
 		flexDirection: "row",
 		colSpan: 5,
 		rowSpan: 4,
-		imgSrc: ""
+		imgSrc: "/class-meet/img/news3.png",
+		year: 4,
+		link: "https://news.detik.com/berita/d-5084356/penemuan-janin-kucing-dikira-bayi-bikin-heboh-warga-di-kabupaten-bekasi"
 	},
 	{
-		title: "Title",
-		content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco.",
+		title: "Melawan Polisi, Residivis Pencuri Motor di Bekasi Ditembak di Kaki",
+		content: "Polisi menangkap seorang pelaku pencurian motor di Kabupaten Bekasi, Jakarta Barat. Pelaku bernama Nurman diketahui merupakan residivis kasus serupa.",
 		flexDirection: "col",
 		colSpan: 3,
 		rowSpan: 3,
-		imgSrc: ""
+		imgSrc: "/class-meet/img/news4.png",
+		year: 4,
+		link: "https://news.detik.com/berita/d-5094590/melawan-polisi-residivis-pencuri-motor-di-bekasi-ditembak-di-kaki"
 	}
 ]
