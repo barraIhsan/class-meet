1c9a4375e30f847836b053e7bac5053576a22030
barraIhsan <57800056+barraIhsan@users.noreply.github.com>
2024-06-11 16:45:03 +0700

    style: modify the tourism section
    Make the image slightly bigger when one of the card hovered, and also
    the card now clickable and will open a google map for that place.

--- src/data/tourism.js
+++ src/data/tourism.js
@@ -3,30 +3,35 @@ export const tourismItems = [
 		title: "Grand Splash Water Park",
 		content: "Wisata taman air yang ada di dalam Komplek Grand Residence Bekasi dengan berbagai fasilitas dan wahana.",
 		imgSrc: "/class-meet/img/tourism1.png",
-		rate: 4.4
+		rate: 4.4,
+		link: "https://maps.app.goo.gl/DkbDDfStzmjy98Nx7"
 	},
 	{
 		title: "Taman Kota Bekasi",
 		content: "Taman Kota Bekasi ditata dengan sangat cantik dan menarik yang membuatnya nyaman untuk dikunjungi.",
 		imgSrc: "/class-meet/img/tourism2.png",
-		rate: 4.5
+		rate: 4.5,
+		link: "https://maps.app.goo.gl/pGx9qwc5v71mayK57"
 	},
 	{
 		title: "Gedung Juang 45 Bekasi",
 		content: "Gedung Juang 45 di Bekasi adalah bangunan bersejarah yang menyaksikan perjuangan dan perkembangan Bekasi dan Indonesia.",
 		imgSrc: "/class-meet/img/tourism3.png",
-		rate: 4.6
+		rate: 4.6,
+		link: "https://maps.app.goo.gl/JLW4uhqTQVPgHvLm6"
 	},
 	{
 		title: "Alun-alun Eduforest Bekasi",
 		content: "Alun-alun Edu Forest ini mengusung konsep ruang terbuka dengan perpaduan alam.",
 		imgSrc: "/class-meet/img/tourism4.png",
-		rate: 4.5
+		rate: 4.5,
+		link: "https://maps.app.goo.gl/Jhw4rzyqGQUqnCu18"
 	},
 	{
 		title: "Situ Hambaro",
 		content: "Keindahan Situ Rawa Hambaro merupakan salah satu Potensi alam yang sangat besar yang terletak di kecamatan Setu,",
 		imgSrc: "/class-meet/img/tourism5.png",
-		rate: 4.5
+		rate: 4.5,
+		link: "https://maps.app.goo.gl/nVGjWZcxbygCuEB28"
 	}
 ]
